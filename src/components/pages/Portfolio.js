ecd78f39d73d7f66ffb5cb49143ceac94b49371f
kassrojas <kassandrarojass@gmail.com>
2023-02-05 21:32:11 -0500

    created projectList js

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,12 +1,56 @@
-import React from 'react';
-import Project from '../Project';
+import React, { useState } from 'react';
+import ProjectList from '../ProjectList';
 
 export default function Portfolio(){
+    const [ apps, setApps ] = useState([
+        { 
+            title: 'Tasteful Choice', 
+            body: 'Uses two server side API\'s to display meals and drinks that contain a user chosen ingredient', 
+            deployed: 'https://alandis01.github.io/food-and-bev-project-1/', 
+            gitLink: 'https://github.com/alandis01/food-and-bev-project-1', 
+            id: 1 
+        },
+        { 
+            title: 'Sofa King Cool', 
+            body: 'An application that serves to provide an area for organizing furniture wants for a room in a user friendly way', 
+            deployed: 'https://quiet-river-37193.herokuapp.com/', 
+            gitLink: 'https://github.com/kassrojas/sofa-king-cool', 
+            id: 2 
+        },
+        { 
+            title: 'Tasteful Choice',
+            body: 'Uses two server side API\'s to display meals and drinks that contain a user chosen ingredient', 
+            deployed: 'https://alandis01.github.io/food-and-bev-project-1/', 
+            gitLink: 'https://github.com/alandis01/food-and-bev-project-1', 
+            id: 3 
+        },
+        { 
+            title: 'Tasteful Choice', 
+            body: 'Uses two server side API\'s to display meals and drinks that contain a user chosen ingredient', 
+            deployed: 'https://alandis01.github.io/food-and-bev-project-1/', 
+            gitLink: 'https://github.com/alandis01/food-and-bev-project-1', 
+            id: 4 
+        },
+        { 
+            title: 'Tasteful Choice', 
+            body: 'Uses two server side API\'s to display meals and drinks that contain a user chosen ingredient', 
+            deployed: 'https://alandis01.github.io/food-and-bev-project-1/', 
+            gitLink: 'https://github.com/alandis01/food-and-bev-project-1', 
+            id: 5 
+        },
+        { 
+            title: 'Tasteful Choice', 
+            body: 'Uses two server side API\'s to display meals and drinks that contain a user chosen ingredient', 
+            deployed: 'https://alandis01.github.io/food-and-bev-project-1/', 
+            gitLink: 'https://github.com/alandis01/food-and-bev-project-1', 
+            id: 6 
+        },
+    ], [])
     return (
         <>
         <main>
             <h1> Portfolio </h1>
-        < Project />
+        < ProjectList apps={apps} heading='All Projects'/>
         </main>
         </>
     )
