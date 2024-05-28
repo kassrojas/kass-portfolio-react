aefe0976348c616e1e6434a53a83285d27d56ea6
Kassandra Lazette <kassandra.lazette@gmail.com>
2024-05-28 08:51:36 -0400

    updated resume

--- src/components/Contact/resume.component.jsx
+++ src/components/Contact/resume.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import KassRojasResume from "../../assets/files/kass_lazette_resume.pdf";
+import KassRojasResume from "../../assets/files/kassandra_lazette_resume.pdf"
 
 const Resume = ({ resumeView }) => {
   return (
